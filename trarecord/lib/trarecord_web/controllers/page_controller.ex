d23a84753404fe02859b5661eed62cae9f1d0cfe
thehaigo <sirannha@gmail.com>
2022-09-21 14:15:09 +0900

    add top page redirect

--- trarecord/lib/trarecord_web/controllers/page_controller.ex
+++ trarecord/lib/trarecord_web/controllers/page_controller.ex
@@ -1,7 +1,17 @@
 defmodule TrarecordWeb.PageController do
   use TrarecordWeb, :controller
 
+  alias Trarecord.Users.User
+
   def index(conn, _params) do
-    render(conn, "index.html")
+    redirect_to(conn, conn.assigns.current_user)
+  end
+
+  def redirect_to(conn, %User{}) do
+    redirect(conn, to: Routes.live_path(TrarecordWeb.Endpoint, TrarecordWeb.HomeLive))
+  end
+
+  def redirect_to(conn, nil) do
+    redirect(conn, to: Routes.user_registration_path(TrarecordWeb.Endpoint, :new))
   end
 end
