88f6cc843e7ab7e2da215bc0ab4748d72054434d
thehaigo <sirannha@gmail.com>
2022-09-24 14:00:43 +0900

    add back and action link slot

--- trarecord/lib/trarecord_web/live/components/navigation.ex
+++ trarecord/lib/trarecord_web/live/components/navigation.ex
@@ -5,11 +5,17 @@ defmodule TrarecordWeb.Components.Navigation do
     ~H"""
     <div class="fixed z-20 navbar bg-primary text-primary-content w-full">
       <div class="navbar-start">
+        <%= if @back do %>
+          <%= live_redirect "Back", to: @back, class: "btn btn-ghost normal-case text-xl" %>
+        <% end %>
       </div>
       <div class="navbar-center">
         <a class="btn btn-ghost normal-case text-4xl"><%= @title %></a>
       </div>
       <div class="navbar-end">
+        <%= if @action do %>
+          <%= @action %>
+        <% end %>
       </div>
     </div>
     """
@@ -23,9 +29,9 @@ defmodule TrarecordWeb.Components.Navigation do
        <span class="btm-nav-label">Home</span>
      </a>
     </button>
-    <button class={if @title == "Routes", do: "active", else: ""}>
-     <a href="/home">
-       <span class="btm-nav-label">Routes</span>
+    <button class={if @title == "Items", do: "active", else: ""}>
+     <a href="/items">
+       <span class="btm-nav-label">Items</span>
      </a>
     </button>
 
